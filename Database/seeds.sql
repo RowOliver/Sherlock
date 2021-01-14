dac96b2902729e19806f21137c7cf7edb0625930
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-01-13 21:20:35 -0800

    readme file started

--- Database/seeds.sql
+++ Database/seeds.sql
@@ -9,6 +9,14 @@ INSERT INTO roles (title, salary, department_id) VALUE ("Software Engineer", 115
 INSERT INTO roles (title, salary, department_id) VALUE ("Accountant", 125000, 2);
 INSERT INTO roles (title, salary,department_id) VALUE ("Analyst", 50000, 3);
 INSERT INTO roles (title,salary,department_id) VALUE ("Sales Lead", 75000, 4);
+INSERT INTO roles (title, salary,department_id) VALUE ("Lead Engineer", 90000, 5);
 
 
-INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("") 
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("Bob", "Belcher",1);
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("Linda", "Belcher",2);
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("Tina", "Belcher",3);
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("Gene", "Belcher",4);
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("Louise", "Belcher",5);
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("Teddy", "Murphy",2);
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("Jimmy", "Pesto", 4,);
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUE ("Mort", "Morten", 1);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Sherlock
+
+Sherlock
+
+Employee tracker that runs on the command line
+
+Description:
+The need for a Content Management Systems 
